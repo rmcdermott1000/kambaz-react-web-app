0e818b2ed4d72badf4429f06af728118be8a9e00
rmcdermott1000 <rmmcdermott1@comcast.net>
2025-01-11 14:27:15 -0500

    final touches on A1

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
       <li><Link to="/Labs/Lab3">Lab 3</Link></li>
       <li><Link to="/Kambaz">Kambaz</Link></li>
+      <li><Link id="wd-github" to="https://github.com/rmcdermott1000/kambaz-react-web-app">GITHUB</Link></li>
     </ul>
   );
 }
